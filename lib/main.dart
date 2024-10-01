037b420ed5abc7044d641a807ba21b93e5306866
Phan Tri <tri_phan@onskyhealth.com>
2024-10-01 13:58:20 +0700

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:spotify/core/configs/theme/app_theme.dart';
-import 'package:spotify/presentation/choose_mode/cubit/theme_cubit.dart';
+import '../core/configs/theme/app_theme.dart';
+import '../presentation/choose_mode/cubit/theme_cubit.dart';
 
 import 'core/routes/app_route.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,6 +16,9 @@ Future<void> main() async {
     storageDirectory: kIsWeb
         ? HydratedStorage.webStorageDirectory
         : await getApplicationDocumentsDirectory(),
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
